fb00c3a0129f2b260ecd7a1f0be596b6520a91bf
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-08-21 14:14:13 -0300

    inserindo media query no header

--- src/Components/Header/headerStyle.jsx
+++ src/Components/Header/headerStyle.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+const mobileBreakpoint = '768px'
 
 export const HeaderBox = styled.div`
 background-color: #9e9e9e1a;
@@ -7,10 +8,18 @@ padding: 1%;
 align-items: center;
 justify-content: space-between;
 border-radius: 5px;
+
+@media (max-width: ${mobileBreakpoint}) {
+width: 29.3rem;
+}
 `
 
 export const H1 = styled.h1`
 font-size: 2rem;
+
+@media (max-width: ${mobileBreakpoint}) {
+font-size: 25px;
+}
 `
 
 export const Ul = styled.ul`
