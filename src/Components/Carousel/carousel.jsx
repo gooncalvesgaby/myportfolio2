928daee1421e17afef090f0d779e2f0b1e275009
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-09-18 16:18:16 -0300

    criação do componente carousel

---
+++ src/Components/Carousel/carousel.jsx
@@ -0,0 +1,11 @@
+import * as S from "./carouselStyle.jsx"
+
+function Carousel() {
+    return(
+        <S.Section>
+
+        </S.Section>
+    )
+}
+
+export default Carousel
--- src/Components/Rotas/rotas.jsx
+++ src/Components/Rotas/rotas.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Inicio from "../Inicio/inicio.jsx"
 import Sobre from "../Sobre/sobre.jsx"
+import Carousel from "../Carousel/carousel.jsx"
 import Header from "../Header/header.jsx"
+
 
 
 export default function Rotas() {
@@ -11,6 +13,7 @@ export default function Rotas() {
             <Routes>
                 <Route element={ <Inicio/> } path="/"/>
                 <Route element={ <Sobre/> } path="/Sobre"/>
+                <Route element={ <Carousel/>} path="/Carousel"/>
             </Routes>
         </BrowserRouter>
     )
