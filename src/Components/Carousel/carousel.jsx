d82146516408ff75c1ea898a3249b62d79aa62cf
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-09-19 21:54:28 -0300

    ajustes nos estilos do carrosel

--- src/Components/Carousel/carousel.jsx
+++ src/Components/Carousel/carousel.jsx
@@ -1,12 +1,12 @@
 import * as S from "./carouselStyle.jsx"
 import { register } from 'swiper/element/bundle'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import Angular from "../../assets/angular.png"
+import myCss from "../../assets/css.png"
 import Js from "../../assets/js.png"
-import Python from "../../assets/python.png"
 import Rct from "../../assets/react.png"
 import Ts from "../../assets/ts.png"
-import Vue from "../../assets/vue.png"
+import myHtml from "../../assets/html.png"
+import sc from "../../assets/sc.png"
 
 register();
 import 'swiper/css';
@@ -17,55 +17,57 @@ import 'swiper/css/scrollbar';
 function Carousel() {
 
     const data = [
-        {id: '1', image:{Angular} },
+        {id: '1', image:{myCss} },
         {id: '2', image:{Js} },
-        {id: '3', image:{Python} },
+        {id: '3', image:{sc} },
         {id: '4', image:{Rct} },
         {id: '5', image:{Ts} },
-        {id: '6', image:{Vue} }
+        {id: '6', image:{myHtml} }
+        
     ]
 
     return(
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={1}
-      pagination={{ clickable: true }}
-      navigation
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide><S.Img
-                        src={Vue}
-                        alt=""
-                        />
-      </SwiperSlide>
-      <SwiperSlide><S.Img
-                        src={Rct}
-                        alt=""
-                        />
-      </SwiperSlide>
-      <SwiperSlide><S.Img
-                        src={Angular}
-                        alt=""
-                        />
-      </SwiperSlide>
-      <SwiperSlide><S.Img
-                        src={Js}
-                        alt=""
-                        />
-      </SwiperSlide>
-      <SwiperSlide><S.Img
-                        src={Ts}
-                        alt=""
-                        />
-      </SwiperSlide>
-      <SwiperSlide><S.Img
-                        src={Python}
-                        alt=""
-                        />
-      </SwiperSlide>
-      
-    </Swiper>
+      <S.CarouselContainer>
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+          navigation
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          <SwiperSlide><S.Img
+                            src={myHtml}
+                            alt="Imagem de marcação HTML"
+                            />
+          </SwiperSlide>
+          <SwiperSlide><S.Img
+                            src={myCss}
+                            alt="Imagem de estilo CSS"
+                            />
+          </SwiperSlide>
+          <SwiperSlide><S.Img
+                            src={Js}
+                            alt="Imagem da linguagem JS"
+                            />
+          </SwiperSlide>
+          <SwiperSlide><S.Img
+                            src={Ts}
+                            alt="Imagem da linguagem TS"
+                            />
+          </SwiperSlide>
+          <SwiperSlide><S.Img
+                            src={Rct}
+                            alt="Imagem do framework React"
+                            />
+          </SwiperSlide>
+          <SwiperSlide><S.Img
+                            src={sc}
+                            alt="Imagem de estilov styled-components"
+                            />
+          </SwiperSlide>
+        </Swiper>
+      </S.CarouselContainer>
     )
 }
 
--- src/Components/Carousel/carouselStyle.jsx
+++ src/Components/Carousel/carouselStyle.jsx
@@ -1,11 +1,16 @@
 import styled from "styled-components"
 
-export const Section = styled.div`
-width: 100%;
-max-width: 1440px;
+export const CarouselContainer = styled.div`
+width: 100%; 
+max-width: 1200px; 
+height: 400px; 
 margin: 0 auto;
+display: flex;
+justify-content: center;
 `
 
 export const Img = styled.img`
-
+width: 100%; 
+height: 60vh; 
+object-fit: contain; 
 `
