a0b95a179ea189942d7511f66c5d77184760aba1
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-10-28 16:51:52 -0300

    ajustes

--- src/Components/Blog/blog.jsx
+++ src/Components/Blog/blog.jsx
@@ -1,11 +1,10 @@
 import * as S from "./blogStyle.jsx"
-
-function myBlog() {
+function Blog() {
     return(
         <S.Blog>
-            <S.H1>Componente My Blog em construção!</S.H1>
+            <S.H1>Componente My Blog em produção!</S.H1>
         </S.Blog>
     )
 }
 
-export default myBlog
+export default Blog
--- src/Components/Blog/blogStyle.jsx
+++ src/Components/Blog/blogStyle.jsx
@@ -1,9 +1,7 @@
 import styled from "styled-components"
 const mobileBreakpoint = '480px'
 
-
 export const Blog = styled.div`
-
 `
 
 export const H1 = styled.h1`
